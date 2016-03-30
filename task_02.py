4574968d520f7f3b2c3f5fc7d157bdc17213d93c
paisely65 <cunysps@gmail.com>
2016-03-30 02:56:00 -0400

    Task 02 is finished.

---
+++ task_02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A small docstring"""
+
+import data
+NIGEL = {'Nigel Tufnel': ['guitar', 'vocals', 'bass', 'violin', 'harmonica','clarinet', 'keyboards', 'piano']}
+BANDS_NAMES = {}
+BAND_NAMES(NIGEL)
+print BAND_NAMES(NIGEL)
