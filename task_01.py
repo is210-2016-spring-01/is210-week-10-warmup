118c1e2548608854c42a97527497593e0113aacb
paisely65 <cunysps@gmail.com>
2016-03-30 02:06:57 -0400

    Task 1 iscompleted.

---
+++ task_01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A small docstring will create a dictionary."""
+
+GRADE_DATA = {
+        'Luke SkyWalker': {'math': 'B', 'etiquette': 'B+', 'grammar': 'B', 'gym': 'A'}
+        'C-3PO': {'math': 'C', 'etiquette': 'A+', 'grammar': 'A', 'gym': 'F'}
+        'Han Solo': {'math': 'A-', 'etiquette': 'C-', 'grammar': 'B', 'gym': 'B'}
+             }
+print GRADE_DATA
