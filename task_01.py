87035edd63803f4f00b19bc92a82e67f500f9b0a
paisely65 <cunysps@gmail.com>
2016-03-30 02:28:40 -0400

    Task 1 is completed

--- task_01.py
+++ task_01.py
@@ -3,8 +3,22 @@
 """A small docstring will create a dictionary."""
 
 GRADE_DATA = {
-        'Luke SkyWalker': {'math': 'B', 'etiquette': 'B+', 'grammar': 'B', 'gym': 'A'}
-        'C-3PO': {'math': 'C', 'etiquette': 'A+', 'grammar': 'A', 'gym': 'F'}
-        'Han Solo': {'math': 'A-', 'etiquette': 'C-', 'grammar': 'B', 'gym': 'B'}
-             }
-print GRADE_DATA
+    'Luke SkyWalker': {
+        'math': 'B',
+        'etiquette': 'B+',
+        'grammar': 'B',
+        'gym': 'A'
+        },
+    'C-3PO': {
+        'math': 'C',
+        'etiquette': 'A+',
+        'grammar': 'A',
+        'gym': 'F'
+        },
+    'Han Solo': {
+        'math': 'A-',
+        'etiquette': 'C-',
+        'grammar': 'B',
+        'gym': 'B'
+        }
+}
