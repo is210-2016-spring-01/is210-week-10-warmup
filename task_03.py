75fbf867b8964329db2b9ce6c4543138029af70f
John Farrell <jf4rr3ll@gmail.com>
2016-03-31 19:05:26 +0000

    Week Ten Warmup

---
+++ task_03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 03 Module"""
+
+import data
+
+CORRECTED = data.BANDS.copy()
+
+CORRECTED['Dylan'] = {'Bob Dylan': ['vocals', 'guitar', 'harmonica']}
+
+del CORRECTED['Van Halen']['David Lee Roth']
+
+CORRECTED['Van Halen']['Sammy Hagar'] = ['vocals']
