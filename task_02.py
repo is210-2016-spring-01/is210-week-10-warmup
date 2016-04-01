9cd498acf0277e8f6b3984b0a8ff614e3f862cb2
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-04-01 02:52:05 +0000

    Added dictionary to access data

---
+++ task_02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A small docstring for accessing data."""
+
+import data
+
+NIGEL = data.BANDS['Spinal Tap']['Nigel Tufnel']
+
+BAND_NAMES = dict.keys(data.BANDS)
