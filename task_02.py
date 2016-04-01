0256615def655825c864c7bbfde3c38d656fbb58
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-04-01 00:59:13 +0000

    am i done?

---
+++ task_02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 02: assigning dictionary values and printing out keys"""
+
+
+import data
+
+NIGEL = data.BANDS['Spinal Tap']['Nigel Tufnel']
+
+BANDS_NAMES = data.BANDS.keys()
---
+++ task_05.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 05: change value without altering or
+creating new dict/variable for data.py"""
+
+
+import data
+
+data.SUPERHEROES['Logan']['alias'] = 'Wolverine'
